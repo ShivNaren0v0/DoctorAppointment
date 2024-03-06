48c7c90c18f0cf18ada1c29e79853e800492dc44
Thanush A  A <aathanush@gmail.com>
2024-03-06 09:35:26 +0530

    Update LoginDTO.java
    Documentation for methods in LoginDTO

--- src/main/java/com/questionmark/doctorappointment/patient/dto/LoginDTO.java
+++ src/main/java/com/questionmark/doctorappointment/patient/dto/LoginDTO.java
@@ -17,17 +17,50 @@ public class LoginDTO {
         this.password = password;
     }
 
+    /**
+     * <h3>getEmail()</h3>
+     * For getting email address stored in the DTO
+     *
+     * @author  Thanush A A
+     * @version 1.0
+     * @since   2024-02-28
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * <h3>setEmail()</h3>
+     * For setting email address variable in the DTO
+     *
+     * @author  Thanush A A
+     * @version 1.0
+     * @since   2024-02-28
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * <h3>getPassword</h3>
+     * For getting password data stored in the DTO
+     *
+     * @author  Thanush A A
+     * @version 1.0
+     * @since   2024-02-28
+     */
     public String getPassword() {
         return password;
     }
+    /**
+     * <h3>setPassword</h3>
+     * For setting a password in the LoginDTO
+     *
+     * @author  Thanush A A
+     * @version 1.0
+     * @since   2024-02-28
+     */
+
 
     public void setPassword(String password) {
         this.password = password;
