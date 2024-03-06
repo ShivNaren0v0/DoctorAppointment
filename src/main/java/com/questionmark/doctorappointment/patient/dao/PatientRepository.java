ace5bf18502e3c2f789d90b2b9405995b7cf84f7
Thanush A  A <aathanush@gmail.com>
2024-03-06 09:39:39 +0530

    Documentation for the findByEmail() method in PatientRepository

--- src/main/java/com/questionmark/doctorappointment/patient/dao/PatientRepository.java
+++ src/main/java/com/questionmark/doctorappointment/patient/dao/PatientRepository.java
@@ -13,5 +13,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient,Integer> {
 
+    /**
+     * <h3>findByEmail(String email)</h3>
+     * Returns the Patient Entity in the repository corresponding to the e-mail ID.
+     * @param email email ID string
+     * @return Patient
+     * @author  Thanush A A
+     * @version 1.0
+     * @since   2024-02-28
+     */
     Patient findByEmail(String email);
 }
