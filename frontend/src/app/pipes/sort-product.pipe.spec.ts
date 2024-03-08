8d6d9f238d3650d021a7f8f9f8c80c5187fa4908
Thanush A  A <aathanush@gmail.com>
2024-03-08 14:48:16 +0530

    Deleted front end

--- frontend/src/app/pipes/sort-product.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SortProductPipe } from './sort-product.pipe';
-
-describe('SortProductPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortProductPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
